2e2cbc0803fb4100751217832f224e574e45a9cf
“wonee09” <wonjang031@gmail.com>
2025-09-15 00:34:12 +0900

    “first”

---
+++ src/app/breeds/[id]/page.jsx
@@ -0,0 +1,30 @@
+import PageContainer from "@/components/common/PageContainer";
+import CatDetail from "@/components/ui/CatDetail";
+import { getCatById } from "@/lib/services/api/catApi";
+import BreedDetailHeader from "./_components/BreedDetailHeader";
+
+export async function generateMetadata({ params, searchParams }, parent) {
+  // read route params
+  const { id } = await params;
+  const cats = await getCatById(id);
+  const cat = cats[0];
+
+  return {
+    title: cat.breeds[0].name,
+    description: cat.breeds[0].description,
+  };
+}
+
+export default async function CatBreedPage({ params }) {
+  console.log("SSR-CatBreedPage");
+  const { id } = await params;
+  const cats = await getCatById(id);
+  const cat = cats[0];
+
+  return (
+    <PageContainer title={cat.breeds[0].name}>
+      <BreedDetailHeader breed={cat.breeds[0]} />
+      <CatDetail cat={cat} />
+    </PageContainer>
+  );
+}
